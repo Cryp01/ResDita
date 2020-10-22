37559ffe653535d527e3fd3371658471d11c4658
Luis <gustavoperez.lr@gmail.com>
2020-10-22 14:19:02 -0400

    listadi clientes

--- Controllers/listado_articulos.js
+++ Controllers/listado_articulos.js
@@ -18,26 +18,23 @@ console.log(e);
     {
         contenido.innerHTML +=  `
 
-     <a href="#" class="list-group-item list-group-item-action flex-column align-items-start">
-    <div class="d-flex w-100 justify-content-between">
-    <img src="../IMG/bg.jpg" width="150px" height="100px" style="margin-right:20px;">
-    <div>
-    <div class="d-flex w-100 justify-content-between">
-      <h7 class="mb-1">${datos.AR_DESCRI}</h7>
-      <small CLASS="text-success">${datos.AR_PREDET}</small>
-    </div>
-    <p class="mb-1 small">Un Texto plano (plain text), son aquellos archivos formados exclusivamente por texto (sólo caracteres), sin ningún formato; es decir, no requieren ser interpretados para leerse (aunque pueden ser procesados en algunos casos). También son llamados archivos de texto llano, simple o sin formato.</p>
-    <small></small>
-    </div>
-    </div>
-  </a>
+        <li>
+        <div class="media">
+
+        <img class="mr-3" src="../IMG/bg.jpg" alt="Generic placeholder image" with="80px" height="100px">
+        <div class="media-body">
+          <h8 class="mt-0 mb-1 text-primary">${datos.AR_DESCRI}</h8><BR>
+          <span class="badge badge-success badge-pill">${datos.AR_PREDET}</span>
+          </div>
+          </div>
+         <small> Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin.</small>
+      </li><br>
        
     
            
       
         `;
     }
-
 
 
 }
